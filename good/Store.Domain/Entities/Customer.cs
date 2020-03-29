f053e5a9870e618e67ef7f540e1fb385eccb5547
Leandro <leandrodbdias@gmail.com>
2020-03-29 12:57:51 -0300

    Refatorando código entidade de Cliente

---
+++ good/Store.Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities
+{
+    public class Customer
+    {
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+    }
+}
