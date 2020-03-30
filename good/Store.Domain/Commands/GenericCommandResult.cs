ed7a52b68a2061b99996620659581062bbc0eb1e
Leandro <leandrodbdias@gmail.com>
2020-03-30 17:54:08 -0300

    GenericCommand Result

---
+++ good/Store.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,18 @@
+using Store.Domain.Commands.Contracts;
+
+namespace Store.Domain.Commands
+{
+    public class GenericCommandResult : ICommandResult
+    {
+        public GenericCommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public object Data { get; private set; }
+    }
+}
